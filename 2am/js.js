2be0f71bde7a7751cc0c7dc41ca1cf8580d86138
3amfuxxk <olezeml-ipt25@lll.kpi.ua>
2023-07-02 05:33:30 +0300

    fisrt front

--- 2am/js.js
+++ 2am/js.js
@@ -1,3 +1,4 @@
+//nav search
 const searchIcon = document.getElementById('searchIcon');
 const searchField = document.getElementById('searchField');
 
@@ -12,9 +13,34 @@ searchIcon.addEventListener('click', function() {
     }
 });
 
+//nav menu
 const menu = document.getElementById('menu');
 const navmenu = document.getElementById('navmenu')
+
 menu.onclick = function() {
     menu.classList.toggle("openmenu");
     navmenu.classList.toggle("visible");
 }
+
+//like icon
+// const heartIcon = document.querySelector('.heart');
+// const strokeChange = document.querySelector('.strokechange');
+// const iconContainer = document.querySelector('.icon1');
+
+// iconContainer.addEventListener('click', function () {
+//     if (strokeChange.classList.contains('active')) {
+//         strokeChange.classList.remove('active');
+//         strokeChange.classList.add('strokechange');
+//     } else {
+//         strokeChange.classList.remove('strokechange');
+//         strokeChange.classList.add('active');
+//     }
+// });
+
+const opttxt = document.getElementById('opttxt');
+
+opttxt.addEventListener('transitionend', () => {
+    opttxt.style.display = 'none';
+    console.log("Transition ended");
+});
+
