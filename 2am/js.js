152fb0f5a6df592a36bc8006398095ca39dc1788
3amfuxxk <olezeml-ipt25@lll.kpi.ua>
2023-07-03 04:10:00 +0300

    front

--- 2am/js.js
+++ 2am/js.js
@@ -15,7 +15,7 @@ searchIcon.addEventListener('click', function() {
 
 //nav menu
 const menu = document.getElementById('menu');
-const navmenu = document.getElementById('navmenu')
+const navmenu = document.getElementById('navmenu');
 
 menu.onclick = function() {
     menu.classList.toggle("openmenu");
@@ -37,10 +37,126 @@ menu.onclick = function() {
 //     }
 // });
 
-const opttxt = document.getElementById('opttxt');
+// const opttxt = document.getElementById('opttxt');
 
-opttxt.addEventListener('transitionend', () => {
+// opttxt.addEventListener('transitionend', () => {
+//     opttxt.style.display = 'none';
+//     console.log("Transition ended");
+// });
+
+
+// const targetElement = document.getElementById("intext");
+// const opttxtElement = document.getElementById("opttxt");
+
+// const observer = new MutationObserver((mutationsList) => {
+//   for (let mutation of mutationsList) {
+//     if (mutation.type === "attributes" && mutation.attributeName === "style") {
+//       const targetHeight = targetElement.offsetHeight;
+//       const opttxtHeight = opttxtElement.offsetHeight;
+
+//       if (targetHeight >= opttxtHeight) {
+//         opttxtElement.style.display = "flex";
+//       } else {
+//         opttxtElement.style.display = "none";
+//       }
+//     }
+//   }
+// });
+
+// const config = {
+//   attributes: true,
+//   attributeFilter: ["style"],
+// };
+
+// observer.observe(targetElement, config);
+
+// const opttxt = document.getElementById("opttxt3");
+// const intext = document.getElementById("intext3");
+// const opttxtsm = document.getElementById("opttxtsm3");
+
+// function expandText() {
+//     opttxtsm.style.fontSize = '48px';
+//     opttxt.style.opacity = '0';
+//     opttxt.style.display = 'none';
+//     intext.style.maxHeight = '350px';
+//     intext.style.opacity = '1';
+// }
+
+// function resetText() {
+//     opttxtsm.style.fontSize = '36px';
+//     intext.style.maxHeight = '0px';
+//     intext.style.opacity = '0';
+//     setTimeout(function() {
+//         opttxt.style.display = 'flex';
+//         opttxt.style.opacity = '1';
+//     }, 350);
+// }
+
+function expandText(opttxtId, intextId, opttxtsmId) {
+    const opttxt = document.getElementById(opttxtId);
+    const intext = document.getElementById(intextId);
+    const opttxtsm = document.getElementById(opttxtsmId);
+
+    opttxtsm.style.fontSize = '48px';
+    opttxt.style.opacity = '0';
     opttxt.style.display = 'none';
-    console.log("Transition ended");
-});
+    intext.style.maxHeight = '350px';
+    intext.style.opacity = '1';
+}
 
+function resetText(opttxtId, intextId, opttxtsmId) {
+    const opttxt = document.getElementById(opttxtId);
+    const intext = document.getElementById(intextId);
+    const opttxtsm = document.getElementById(opttxtsmId);
+
+    opttxtsm.style.fontSize = '36px';
+    intext.style.maxHeight = '0px';
+    intext.style.opacity = '0';
+    setTimeout(function() {
+        opttxt.style.display = 'flex';
+        opttxt.style.opacity = '1';
+    }, 350);
+}
+
+// function addAfterElement() {
+//     var covering = document.getElementById('covering3');
+//     var afterElement = document.createElement('div');
+//     afterElement.classList.add('after-element');
+//     covering.appendChild(afterElement);
+// }
+
+// function removeAfterElement() {
+//     var covering = document.getElementById('covering3');
+//     var afterElement = covering.querySelector('.after-element');
+//     afterElement.style.opacity = '0';
+//     afterElement.addEventListener('transitionend', function() {
+//       afterElement.remove();
+//     });
+//   }
+
+// var bg3 = document.querySelector('.bg3');
+// var covering = document.querySelector('.covering');
+
+// bg3.addEventListener('mouseenter', function() {
+//     covering.classList.add('show');
+// });
+
+// bg3.addEventListener('mouseleave', function() {
+//     covering.classList.remove('show');
+// });
+
+var coveringElements = document.querySelectorAll('[id^="covering"]');
+var bgElements = document.querySelectorAll('[id^="bg"]');
+
+// Додаємо обробник події для кожного елемента .bg
+bgElements.forEach(function(bgElement, index) {
+  var coveringElement = coveringElements[index];
+  
+  bgElement.addEventListener('mouseenter', function() {
+    coveringElement.classList.add('show');
+  });
+  
+  bgElement.addEventListener('mouseleave', function() {
+    coveringElement.classList.remove('show');
+  });
+});
